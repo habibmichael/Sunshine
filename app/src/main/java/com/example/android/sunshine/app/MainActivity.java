402dcca358ee7437940024e5cbb314735e87662d
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-09-19 23:10:58 -0700

    Create menu item for displaying prefered location on a map

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -54,8 +57,33 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(this,SettingsActivity.class));
             return true;
         }
+        if(id==R.id.action_map){
+            openPreferedLocationInMap();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferedLocationInMap(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = prefs.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+
+        //create geo location uri from prefred locaiton
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q",location).build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        //start activity  is succesful
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }else{
+            //TODO log error message
+        }
+
     }
 }
 
